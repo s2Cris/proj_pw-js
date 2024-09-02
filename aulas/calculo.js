c137df4bf83268469e6e8ea836ad923946041ef1
Cristian <cristian.marcheti@etec.sp.gov.br>
2024-09-02 18:27:30 -0300

    introdução ao java script

---
+++ aulas/calculo.js
@@ -0,0 +1,7 @@
+function somar () {
+    var x =2
+    var y =2
+    let res = x+y
+    document.getElementById("soma").innerHTML=res
+    document.getElementById("conf-soma").innerHTML="Calculado ✔"
+}
