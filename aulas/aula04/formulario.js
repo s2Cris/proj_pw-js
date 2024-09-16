f0e5f1c2592bfa58c956772d321fc75f45219102
cris <cristian.marcheti@etec.sp.gov.br>
2024-09-16 18:33:22 -0300

    correção e console js

--- aulas/aula04/formulario.js
+++ aulas/aula04/formulario.js
@@ -7,32 +7,31 @@ function adicionarlinha(){
     const demissao = document.getElementById("demissao").value;
 
     //Se for preenchido vazio
-    if(nome === "" || idade === "" || admissao === ""){
-    alert("Preencha os valores do formulário!")
+    if(nome === '' || idade === '' || admissao === ''){
+        alert("Preencha os valores do formulário!");
+    } else{
+        //Cria uma linha da tabela se não existir
+        const tabela = document.getElementById("tabelaDados").getElementsByTagName('tbody')[0];
+
+        //Inserindo uma nova linha
+        const novaLinha=tabela.insertRow();
+    
+        //Inserindo valores da linha 
+        const celId = novaLinha.insertCell(0);
+        const celNome = novaLinha.insertCell(1);
+        const celIdade = novaLinha.insertCell(2);
+        const celAdmissao = novaLinha.insertCell(3);
+        const celDemissao = novaLinha.insertCell(4);
+    
+        //Inserindo os valores dentro das células
+        celId.innerHTML = proximoId;
+        celNome.innerHTML = nome;
+        celIdade.innerHTML = idade;
+        celAdmissao.innerHTML = admissao;
+        celDemissao.innerHTML = demissao;
+     
+        proximoId++
+    
+        //Limpar o formulário
+        document.getElementById("linhaForm").reset();}
     }
-
-    //Cria uma linha da tabela se não existir
-    const tabela = document.getElementById("tabelaDados").getElementsByTagName("tbody")[0];
-
-    //Inserindo uma nova linha
-    const novaLinha = tabela.inserBow
-
-    //Inserindo valores da linha 
-    const celId = novaLinha.insertCell(0);
-    const celnome = novaLinha.insertCell(1);
-    const celIdade = novaLinha.insertCell(2);
-    const celAdmissao = novaLinha.insertCell(3);
-    const celDemissao = novaLinha.insertCell(4);
-
-    //Inserindo os valores dentro das células
-    celId.innerHTML = proximoID;
-    celNome.innerHTML = nome;
-    celIdade.innerHTML = idade;
-    celAdmissao.innerHTML = admissao;
-    celDemissao.innerHTML = demissao;
- 
-    proximo++
-
-    //Limpar o formulário
-    document.getElementById("linhaForm").reset();
-}
